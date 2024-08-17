98ed81f0da9d3631f070b501b978063c0155a19c
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-17 20:57:37 +0700

    feat(sa) : sa routes n apply middleware

--- src/application/web.ts
+++ src/application/web.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { publicRouter } from '../route/public-api';
 import { errorMiddleware } from '../middleware/error-middleware';
 import { setupSwagger } from './swagger';
+import { superadminRoute } from '../route/superadmin-api';
 
 export const web = express();
 
@@ -9,4 +10,5 @@ web.use(express.json());
 setupSwagger(web);
 
 web.use('/v1/api', publicRouter);
+web.use('/v1/api/superadmin', superadminRoute);
 web.use(errorMiddleware);
