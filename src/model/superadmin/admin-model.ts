b0fe150ba140df452104050e9e71e23647b0d0a8
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-18 02:23:36 +0700

    fix(sa): new interface for detail n edit response

--- src/model/superadmin/admin-model.ts
+++ src/model/superadmin/admin-model.ts
@@ -11,7 +11,7 @@ export interface CreateRequest extends CreateResponse {
 }
 
 export interface CreateResponse {
-  id: number;
+  id?: number;
   firstName: string;
   lastName: string;
   phoneNumber: string;
@@ -19,6 +19,30 @@ export interface CreateResponse {
   role: $Enums.Role | RoleAdmin;
   photo?: string | null;
   isActive?: boolean;
+}
+
+export interface DetailResponse extends CreateResponse {
+  createdBy?: string | null;
+  createdAt?: Date | null;
+  updatedBy?: string | null;
+  updatedAt?: Date | null;
+}
+
+export interface DetailRequest extends Admin {
+  createdByAdmin?: Admin | null;
+  updatedByAdmin?: Admin | null;
+}
+
+export interface EditRequest {
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
+  email?: string;
+  role?: $Enums.Role | RoleAdmin;
+  photo?: string | null;
+  isActive?: boolean;
+  updatedBy?: number;
+  password?: string;
 }
 
 export interface PaginationRequest {
@@ -50,5 +74,24 @@ export const toCreateResponse = (user: Admin): CreateResponse => {
     role,
     photo,
     isActive,
+  };
+};
+
+export const toDetailResponse = (user: DetailRequest): DetailResponse => {
+  const { id, firstName, lastName, phoneNumber, email, role, photo, isActive, createdByAdmin, updatedByAdmin, createdAt, updatedAt } = user;
+
+  return {
+    id,
+    firstName,
+    lastName,
+    phoneNumber,
+    email,
+    role,
+    photo,
+    isActive,
+    createdBy: createdByAdmin ? `${createdByAdmin.firstName} ${createdByAdmin.lastName}` : null,
+    updatedBy: updatedByAdmin ? `${updatedByAdmin.firstName} ${updatedByAdmin.lastName}` : null,
+    createdAt,
+    updatedAt,
   };
 };
