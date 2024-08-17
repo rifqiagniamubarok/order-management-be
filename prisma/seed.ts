a6273e47f5565d6e1df17d30a6af51819feeaf02
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-17 14:15:30 +0700

    Feat (superadmin): account seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client';
+import { hash } from 'bcryptjs';
+const prisma = new PrismaClient();
+async function main() {
+  const password = await hash('Superadmin', 10);
+  const superadmin = await prisma.admin.upsert({
+    where: { email: 'superadmin@yopmail.com', phoneNumber: '6285151152323' },
+    update: {},
+    create: {
+      firstName: 'superadmin',
+      lastName: 'superadmin',
+      phoneNumber: '6285151152323',
+      email: 'superadmin@yopmail.com',
+      password,
+    },
+  });
+
+  console.log({ superadmin });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
