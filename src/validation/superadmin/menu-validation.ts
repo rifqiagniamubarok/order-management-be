903d1fa2606f9f38efbef58e5e4bcb3b101b3236
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-21 14:58:02 +0700

    feat(sa): add validation non negative

--- src/validation/superadmin/menu-validation.ts
+++ src/validation/superadmin/menu-validation.ts
@@ -3,7 +3,7 @@ import { z, ZodType } from 'zod';
 export class MenuManagementValidation {
   static readonly CREATE: ZodType = z.object({
     name: z.string().min(1).max(100),
-    price: z.number().min(0),
+    price: z.number().min(0).nonnegative(),
     isAvailable: z.boolean().default(true),
   });
 }
