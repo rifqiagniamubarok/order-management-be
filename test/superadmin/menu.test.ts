2dafa29c2db10546d87fbdadcae8c0d986cf1738
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-24 09:28:46 +0700

    test(sa, menu): menu option

--- test/superadmin/menu.test.ts
+++ test/superadmin/menu.test.ts
@@ -7,6 +7,7 @@ describe('POST superadmin/menu', () => {
   const baseUrl = '/v1/api/superadmin/menu';
   let token = '';
   let id: number;
+  let idOption: number;
   let idForNotFound: number = 1000;
 
   beforeAll(async () => {
@@ -19,6 +20,7 @@ describe('POST superadmin/menu', () => {
 
   afterAll(async () => {
     await MenuTest.delete(id);
+    await MenuTest.deleteOption(id);
   });
 
   it('should reject create table if no authorization', async () => {
@@ -95,6 +97,33 @@ describe('POST superadmin/menu', () => {
       .send();
 
     logger.debug(response.body);
+    expect(response.status).toBe(404);
+    expect(response.body.errors).toBeDefined();
+  });
+  it('should create table if request valid', async () => {
+    const response = await supertest(web)
+      .post(`${baseUrl}/option`)
+      .set({ authorization: 'Bearer ' + token })
+      .send({
+        menuId: id,
+        name: 'Which sausage?',
+      });
+
+    logger.error(response.body);
+    expect(response.status).toBe(201);
+    expect(response.body.data.name).toBe('Which sausage?');
+    idOption = response.body.data.id;
+  });
+  it('should error table if id menu invalid', async () => {
+    const response = await supertest(web)
+      .post(`${baseUrl}/option`)
+      .set({ authorization: 'Bearer ' + token })
+      .send({
+        menuId: idForNotFound,
+        name: 'Which sausage?',
+      });
+
+    logger.error(response.body);
     expect(response.status).toBe(404);
     expect(response.body.errors).toBeDefined();
   });
