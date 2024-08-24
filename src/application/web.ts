69e354f069f208b0b23778a550d24cfb5ddd939e
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-24 22:42:41 +0700

    feat(c): customer route

--- src/application/web.ts
+++ src/application/web.ts
@@ -3,6 +3,7 @@ import { publicRouter } from '../route/public-api';
 import { errorMiddleware } from '../middleware/error-middleware';
 import { setupSwagger } from './swagger';
 import { superadminRoute } from '../route/superadmin-api';
+import { customerRoute } from '../route/customer-api';
 
 export const web = express();
 
@@ -11,4 +12,5 @@ setupSwagger(web);
 
 web.use('/v1/api', publicRouter);
 web.use('/v1/api/superadmin', superadminRoute);
+web.use('/v1/api/customer', customerRoute);
 web.use(errorMiddleware);
