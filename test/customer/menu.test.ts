0f5018975ec23499503f08559ca2e7191cdf78b0
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-08-25 01:36:12 +0700

    test(c,menu): detail menu

--- test/customer/menu.test.ts
+++ test/customer/menu.test.ts
@@ -6,24 +6,35 @@ import { logger } from '../../src/application/logging';
 describe('POST customer/menu', () => {
   const baseUrl = '/api/v1/customer/menu';
   let token: string;
+  let id: number;
 
   beforeAll(async () => {
     const getToken = await CustomerTest.getToken();
+    console.log({ getToken });
     token = 'Bearer ' + getToken;
   });
 
   it('should reject get all menu if token is invalid', async () => {
-    const response = await supertest(web).get(`${baseUrl}/`).send();
+    const response = await supertest(web).get(`${baseUrl}`).send();
 
     logger.debug(response.body);
     expect(response.status).toBe(401);
     expect(response.body.errors).toBeDefined();
   });
   it('should get all menu if request is valid', async () => {
-    const response = await supertest(web).get(`${baseUrl}/`).set({ authorization: token }).send();
+    const response = await supertest(web).get(`${baseUrl}`).set({ authorization: token }).send();
 
     logger.debug(response.body);
     expect(response.status).toBe(200);
     expect(response.body.data).toBeDefined();
+    id = response.body.data[0].id;
+  });
+  it('should get detail menu if request is valid', async () => {
+    const response = await supertest(web).get(`${baseUrl}/${id}`).set({ authorization: token }).send();
+
+    logger.debug(response.body);
+    console.log({ res: response.body });
+    expect(response.status).toBe(200);
+    expect(response.body.data.id).toBe(id);
   });
 });
